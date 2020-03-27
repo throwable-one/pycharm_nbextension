4a7e572314360440aa66f9b1255f83a4a38481bf
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2020-03-27 14:28:44 +0300

    fix install

--- install.py
+++ install.py
@@ -1,3 +1,3 @@
 import os
-os.system("jupyter nbextension install pycharm_nbextension/pycharm_nbextension")
-os.system("jupyter nbextension enable pycharm_nbextension/pycharm_nbextension/pycharm_nbextension")
+os.system("jupyter nbextension install --user pycharm_nbextension/pycharm_nbextension")
+os.system("jupyter nbextension enable --user pycharm_nbextension/pycharm_nbextension/pycharm_nbextension")
---
+++ uninstall.py
@@ -0,0 +1,3 @@
+import os
+os.system("jupyter nbextension uninstall --user pycharm_nbextension/pycharm_nbextension")
+os.system("jupyter nbextension disable --user pycharm_nbextension/pycharm_nbextension/pycharm_nbextension")
